45ed10d678248b8a9e7f36e0de8ecda6d990c60d
Vishal Row Mysore <visrow@gmail.com>
2024-04-01 19:31:45 -0400

    kubernetes and ai integration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,19 @@ import lombok.extern.java.Log;
 
 @Log
 public class Main {
+
+    /**
+     * Execute this with following prompts
+     * "Create a new namespace named 'development'."
+     * "Deploy the latest version of the 'nginx' container in the 'production' namespace."
+     * "Update the 'backend' deployment in the 'staging' namespace to use the image 'backend:v2'."
+     *  "Show me all pods in the 'development' namespace."
+     *  "Show me the logs from the 'backend-12345' pod in the 'production' namespace."
+     * @param args
+     * @throws AIProcessingException
+     */
     public static void main(String[] args) throws AIProcessingException {
         ActionProcessor processor = new ActionProcessor();
-        log.info(processor.processSingleAction("can you provide me list of core V1 component status").toString());
+        log.info(processor.processSingleAction("can you provide me list of core V1 component status","listCoreV1ConfigMapForAllNamespaces").toString());
     }
 }
